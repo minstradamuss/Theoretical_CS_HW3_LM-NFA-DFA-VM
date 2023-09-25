fe3c04cdc2b47a97121d030613e97474cbc894e0
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2023-09-25 12:04:40 +0300

    add

--- backtracking.h
+++
@@ -1,9 +0,0 @@
-#ifndef BACKTRACKING_H
-#define BACKTRACKING_H
-
-// backtracking-based regex matching functions
-int recursive(struct Inst *pc, char *sp);
-int recursiveloop(struct Inst *pc, char *sp);
-int backtrackingvm(struct Inst *prog, char *input);
-
-#endif
