de46ae31d532ca322fb3446e6339420c98ec4cf3
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2023-10-05 21:17:44 +0300

    Move to Kotlin, add tests

---
+++ TSK1/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("junit:junit:4.13.1")
+    implementation("org.testng:testng:7.1.0")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
